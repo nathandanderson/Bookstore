d239f99bef5df797a176997c05fdcaf1a9b95745
Nathan Anderson <andernat@byu.edu>
2022-03-06 13:53:39 -0700

    Cart Icon

---
+++ Bookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Components;
+using Bookstore.Models;
+
+namespace Bookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent (Basket b)
+        {
+            basket = b;
+        }
+   
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    } 
+}
